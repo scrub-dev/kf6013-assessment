4541b9473baa624bda5992f152c188142cee4ecf
Scrub <scott-donaldson@outlook.com>
2022-05-01 20:03:40 +0100

    Weather now has error output

--- assets/js/getWeather.js
+++ assets/js/getWeather.js
@@ -6,27 +6,28 @@ const getWeather = location => {
             + `?lat=${location.lat}`
             + `&lng=${location.lng}`,
             res => {
-              parseWeatherData(res)
+              parseWeatherData(res, location)
             }
   )
 }
 
 //Parsing weather data into either an error or generating the weather widget
-const parseWeatherData = weather => {
+const parseWeatherData = (weather, location) => {
   if(weather.status !== undefined){
-    $("#weatherWidget").html(`<li>${capitalizeFirstChar(weather.status)}</li>`)
+    $("#weatherWidget").html(`<li><a>No Weather data for this location</a></li>`)
   }else{
-    $("#weatherWidget").html(generateWeatherWidget(weather.weatherObservation))
+    $("#weatherWidget").html(generateWeatherWidget(weather.weatherObservation, location))
   }
 }
 
 //converts weather data into human readable widgets in a list.
-const generateWeatherWidget = weather => {
+const generateWeatherWidget = (weather, location) => {
   const PREFIX = "ww"
   return `
-  <li><a id=${PREFIX}clouds>Clouds: ${weather.clouds}</a></li>
-  <li><a id=${PREFIX}humidity>Humidity: ${weather.humidity}</a></li>
-  <li><a id=${PREFIX}temp>Temp: ${weather.temperature}</a></li>
+  <li><a id="${PREFIX}clouds">Clouds: ${weather.clouds}</a></li>
+  <li><a id="${PREFIX}humidity">Humidity: ${weather.humidity}%</a></li>
+  <li><a id="${PREFIX}temp">Temp: ${weather.temperature} c</a></li>
+  <li><a id="${PREFIX}name">${location.name}</a></li>
   `
 }
 
